d961d5f8aa990c7995d87ae34bade42c013a568a
chris6896 <159868610+chris6896@users.noreply.github.com>
2024-10-07 20:24:31 -0400

    Added Text Input field + button

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,59 @@ class TaskList extends StatefulWidget {
 
 class _TaskListScreenState extends State<TaskList> {
   List<Map<String, dynamic>> tasks = [];
-
+  TextEditingController taskController = TextEditingController();
+  void addTask() {
+    if (taskController.text.isNotEmpty) {
+      setState(() {
+        tasks.add({
+          'name': taskController.text,
+          'isCompleted': false,
+        });
+      });
+      taskController.clear();
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Tasks"),
       ),
-      body: Center(child: Text("No tasks")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(9.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: taskController,
+                    decoration: InputDecoration(
+                      labelText: "Enter Task".
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: addTask,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: tasks.isEmpty
+                ? Center(child: Text("No Tasks"))
+                : ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index){
+                    return ListTitle(
+                      title: Text(tasks[index]['name']),
+                    );
+                  },
+                ),
+          )
+        ]
+      ),
     );
   }
 }
